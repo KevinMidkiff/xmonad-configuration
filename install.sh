d2856d4528ef26628fe950284b0907b986e1b56f
Kevin Midkiff <midkiff.km@gmail.com>
2017-03-19 10:07:53 -0700

    Added simple install script, added battery level to xmobarrc

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s ~/.xmonad-configuration/xmonad ~/.xmonad
+ln -s ~/.xmonad-configuration/xmobarrc ~/.xmobarrc
+ln -s ~/.xmonad-configuration/stalonetrayrc ~/.stalonetrayrc
+ln -s ~/.xmonad-configuration/xsessionrc ~/.xsessionrc 
